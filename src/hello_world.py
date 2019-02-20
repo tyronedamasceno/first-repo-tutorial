2699463adedfc036d49b64036eb81e9dd5cc2bbb
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-02-19 22:54:47 -0300

    Incrementando o arquivo hello_world.py

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,4 +1,6 @@
 # Arquivo de hello world para o tutorial de GIT
 
 print('Olá Mundo!!!')
-print('Este código está no meu primeiro repositório GIT!')
+print('Este código está no meu primeiro repositório GIT! E ele já está versionado no GITHUB')
+print('Se quiser conferir é só acessar:')
+print('\nhttps://github.com/tyronedamasceno/first-repo-tutorial\n')
